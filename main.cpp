04dd95aac86499db65ca4cf4a52b468665078483
Z <a@b.c>
2024-08-29 05:09:31 -0600

    Fixed up formatting

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ using deferred = std::function<void()>;
 int main() {
   // Language feature when sadge.
   std::stack<deferred> defer;
-#define DEFER(func) defer.push([&]() { func; })
+  #define DEFER(func) defer.push([&]() { func; })
 
   Logging::info("GLFW initialization.");
   glfwInit();
@@ -37,20 +37,24 @@ int main() {
 
   bool framebufferResized = false;
   glfwSetWindowUserPointer(window, &framebufferResized);
-  glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width,
-                                            int height) {
+  glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height) {
     auto *resized = reinterpret_cast<bool *>(glfwGetWindowUserPointer(window));
     *resized = true;
     Logging::info("GLFW window was resized.");
   });
 
-  DEFER(glfwDestroyWindow(window); glfwTerminate();
-        Logging::info("GLFW destroyed"););
+  DEFER(
+    glfwDestroyWindow(window); 
+    glfwTerminate();
+    Logging::info("GLFW destroyed");
+  );
 
   Logging::info("Vulkan initialization.");
   auto instance = Vulkan::createInstance();
-  DEFER(vkDestroyInstance(instance, nullptr);
-        Logging::info("Vulkan destroyed."););
+  DEFER(
+    vkDestroyInstance(instance, nullptr);
+    Logging::info("Vulkan destroyed.");
+  );
 
   if (instance == VK_NULL_HANDLE) {
     Logging::failure("Unable to create a vulkan instance.");
@@ -64,15 +68,13 @@ int main() {
     return -1;
   }
 
-  auto physicalDevice =
-      Vulkan::pickPhysicalDevice(instance, surface, requiredDeviceExtensions);
+  auto physicalDevice = Vulkan::pickPhysicalDevice(instance, surface, requiredDeviceExtensions);
   if (physicalDevice == VK_NULL_HANDLE) {
     Logging::failure("Could not acquire a valid physical device.");
     return -1;
   }
 
-  auto [logicalDevice, graphicsQueue] =
-      Vulkan::createLogicalDevice(physicalDevice, requiredDeviceExtensions);
+  auto [logicalDevice, graphicsQueue] = Vulkan::createLogicalDevice(physicalDevice, requiredDeviceExtensions);
   DEFER(vkDestroyDevice(logicalDevice, nullptr));
   if (logicalDevice == VK_NULL_HANDLE || graphicsQueue == VK_NULL_HANDLE) {
     Logging::failure("Could not create a logical device.");
@@ -96,11 +98,11 @@ int main() {
     return -1;
   }
 
-  auto [graphicsPipelineLayout, graphicsPipeline] =
-      Vulkan::createGraphicsPipeline(logicalDevice, renderPass);
+  auto [graphicsPipelineLayout, graphicsPipeline] = Vulkan::createGraphicsPipeline(logicalDevice, renderPass);
   DEFER(
-      vkDestroyPipeline(logicalDevice, graphicsPipeline, nullptr);
-      vkDestroyPipelineLayout(logicalDevice, graphicsPipelineLayout, nullptr));
+    vkDestroyPipeline(logicalDevice, graphicsPipeline, nullptr);
+    vkDestroyPipelineLayout(logicalDevice, graphicsPipelineLayout, nullptr)
+  );
   if (graphicsPipelineLayout == VK_NULL_HANDLE ||
       graphicsPipeline == VK_NULL_HANDLE) {
     Logging::failure("Failed to create graphics pipeline.");
@@ -114,8 +116,7 @@ int main() {
     return -1;
   }
 
-  auto commandBuffers = Vulkan::createCommandBuffers(logicalDevice, commandPool,
-                                                     MAX_FRAMES_IN_FLIGHT);
+  auto commandBuffers = Vulkan::createCommandBuffers(logicalDevice, commandPool, MAX_FRAMES_IN_FLIGHT);
   for (auto &commandBuffer : commandBuffers) {
     if (commandBuffer == VK_NULL_HANDLE) {
       Logging::failure("Failed to create command buffers.");
@@ -123,24 +124,33 @@ int main() {
     }
   }
 
-  auto maybeSynchronizers =
-      Vulkan::createFrameSyncObjects(logicalDevice, MAX_FRAMES_IN_FLIGHT);
+  auto maybeSynchronizers = Vulkan::createFrameSyncObjects(logicalDevice, MAX_FRAMES_IN_FLIGHT);
   if (!maybeSynchronizers) {
     Logging::failure("Failed to create synchronization objects.");
     return -1;
   }
   auto synchronizers = maybeSynchronizers.value();
-  DEFER(for (auto synchronizer
-             : synchronizers) { synchronizer.destroy(logicalDevice); });
+  DEFER(
+    for (auto synchronizer : synchronizers) { 
+      synchronizer.destroy(logicalDevice); 
+      }
+  );
 
   uint32_t currentFrame = 0;
   // PRIMARY LOOP
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
     bool frameSuccessful = Vulkan::drawFrame(
-        physicalDevice, logicalDevice, graphicsPipeline, graphicsQueue,
-        swapChain, renderPass, commandBuffers[currentFrame],
-        synchronizers[currentFrame], framebufferResized);
+      physicalDevice, 
+      logicalDevice, 
+      graphicsPipeline, 
+      graphicsQueue,
+      swapChain, 
+      renderPass, 
+      commandBuffers[currentFrame],
+      synchronizers[currentFrame], 
+      framebufferResized
+    );
 
     if (!frameSuccessful) {
       Logging::failure("Failed to draw frame.");
